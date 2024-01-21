fbc7d665dc4964e866c1a28c7a9c743f87cf2023
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-01-20 16:31:20 -0800

    moved files to make it cleaner

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from process_contacts import ProcessContacts
+from contacts.process_contacts import ProcessContacts
 
 app = Flask(__name__)
 CORS(app)
